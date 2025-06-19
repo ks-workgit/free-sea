00f5bd229631536f0f240abdcef0bcfaa6dd1f3d
s_work <hmj2422017@stu.o-hara.ac.jp>
2025-06-19 15:59:13 +0900

    鉱石を追加。

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -4,11 +4,27 @@ using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
+	public static Inventory m_instance;
+	private InventoryUI m_inventoryUI;
+
+	// アイテムリスト
+	public List<OreSetting> m_oreList = new List<OreSetting>();
+
 	[SerializeField] GameObject m_inventory;
 	private bool m_isOpen;
 
+	private void Awake()
+	{
+		if (m_instance == null)
+		{
+			m_instance = this;
+		}
+	}
+
 	private void Start()
 	{
+		m_inventoryUI = GetComponent<InventoryUI>();
+
 		m_isOpen = true;
 		m_inventory.SetActive(false);
 	}
--- Assets/Scripts/OreType.cs
+++ Assets/Scripts/OreType.cs
@@ -12,4 +12,5 @@ public enum OreType
 	Luminite,
 	Crystal,
 	RoseShard,
+	LapisLazuli,
 }
